5722145d587e190e5005077ee08abce64938f44d
Eugeniu Nicolenco <eugeniu.nicolenco@tekoway.com>
2020-07-25 18:01:43 +0300

    removed encapsulation

--- js/jsoneditor_demo.js
+++ js/jsoneditor_demo.js
@@ -1,90 +1,84 @@
-(function () {
-    var extendObj = function (destination) {
-        var source, i, property
-        for (i = 1; i < arguments.length; i++) {
-            source = arguments[i]
-            for (property in source) {
-                if (!source.hasOwnProperty(property)) continue
-                if (source[property] && typeof source[property] === 'object' && source[property] !== null) {
-                    if (!destination.hasOwnProperty(property)) destination[property] = {}
-                    extendObj(destination[property], source[property])
-                } else {
-                    destination[property] = source[property]
-                }
+var extendObj = function (destination) {
+    var source, i, property;
+    for (i = 1; i < arguments.length; i++) {
+        source = arguments[i];
+        for (property in source) {
+            if (!source.hasOwnProperty(property)) continue;
+            if (source[property] && typeof source[property] === 'object' && source[property] !== null) {
+                if (!destination.hasOwnProperty(property)) destination[property] = {};
+                extendObj(destination[property], source[property]);
+            } else {
+                destination[property] = source[property];
             }
         }
-        return destination
     }
+    return destination;
+}
 
-    // Theme to use in ACE editor instances
-    var aceTheme = 'ace/theme/monokai'
+// Theme to use in ACE editor instances
+var aceTheme = 'ace/theme/monokai';
 
-    // ACE Editor placeholders
-    var jeEditSchema = document.querySelector('#schema')
-    var jeOutput = document.querySelector('#output') // Form output
+// ACE Editor placeholders
+var jeEditSchema = document.querySelector('#schema');
+var jeOutput = document.querySelector('#output'); // Form output
 
-    // ACE Editor instances
-    var aceSchemaEditor
-    var aceOutputEditor
+// ACE Editor instances
+var aceSchemaEditor;
+var aceOutputEditor;
 
-    // Split panels
-    var jeSplitCfg = {
-        sizes: [75, 25],
-        minSize: [200, 200],
-        gutterSize: 4
+// Split panels
+var jeSplitCfg = {
+    sizes: [75, 25],
+    minSize: [200, 200],
+    gutterSize: 4
+};
+var jeSplitPanels = [
+    ['#split-panel1'],
+    ['#split-panel5']
+];
+
+
+// filter function for objects
+Object.filter = function (obj, predicate) {
+    var result = {}, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key) && predicate(obj[key])) result[key] = obj[key];
     }
-    var jeSplitPanels = [
-        ['#split-panel1'],
-        ['#split-panel5']
-    ]
+    return result;
+}
 
+// Create ACE Editor instance
+var createEditor = function (el, options) {
+    var replaceCmd = {
+        name: 'replace',
+        bindKey: {win: 'Ctrl-R', mac: 'Command-Option-F'},
+        exec: function (editor) {
+            window.ace.config.loadModule('ace/ext/searchbox', function (e) {
+                e.Search(editor, true)
+            })
+        },
+        readOnly: true
+    }, ed = window.ace.edit(el);
 
-    // filter function for objects
-    Object.filter = function (obj, predicate) {
-        var result = {}, key
-        for (key in obj) {
-            if (obj.hasOwnProperty(key) && predicate(obj[key])) result[key] = obj[key]
-        }
-        return result
-    }
+    ed.setOptions({theme: aceTheme});
+    ed.session.setOptions(extendObj({}, {
+        tabSize: 2,
+        useSoftTabs: true
+    }, options));
+    ed.renderer.setOptions({minLines: 40, maxLines: 40});
+    // Change replace shortcut from Ctrl-H to Ctrl-R
+    ed.commands.addCommand(replaceCmd);
 
-    // Create ACE Editor instance
-    var createEditor = function (el, options) {
-        var replaceCmd = {
-            name: 'replace',
-            bindKey: {win: 'Ctrl-R', mac: 'Command-Option-F'},
-            exec: function (editor) {
-                window.ace.config.loadModule('ace/ext/searchbox', function (e) {
-                    e.Search(editor, true)
-                })
-            },
-            readOnly: true
-        }, ed = window.ace.edit(el)
+    return ed;
+}
 
-        ed.setOptions({theme: aceTheme})
-        ed.session.setOptions(extendObj({}, {
-            tabSize: 2,
-            useSoftTabs: true
-        }, options))
-        ed.renderer.setOptions({minLines: 40, maxLines: 40})
-        // Change replace shortcut from Ctrl-H to Ctrl-R
-        ed.commands.addCommand(replaceCmd)
+// Setup ACE editor for editing Schema
+aceSchemaEditor = createEditor(jeEditSchema, {mode: 'ace/mode/json'});
 
-        return ed
-    }
+// Setup ACE editor for editing output values
+aceOutputEditor = createEditor(jeOutput, {mode: 'ace/mode/json'});
 
-    // Setup ACE editor for editing Schema
-    aceSchemaEditor = createEditor(jeEditSchema, {mode: 'ace/mode/json'})
-
-    // Setup ACE editor for editing output values
-    aceOutputEditor = createEditor(jeOutput, {mode: 'ace/mode/json'})
-
-    // Set resizable split panels
-    window.Split(jeSplitPanels[0], jeSplitCfg)
-    window.Split(jeSplitPanels[1], jeSplitCfg)
-
-    setInterval(() => {
-        console.log({string: aceSchemaEditor.getValue(0), json: JSON.parse(aceSchemaEditor.getValue(0))})
-    }, 2000);
-})()
+// Set resizable split panels
+window.Split(jeSplitPanels[0], jeSplitCfg);
+window.Split(jeSplitPanels[1], jeSplitCfg);
 
